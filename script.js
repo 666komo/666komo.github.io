8e223f4d6b2ae96aba16c56c6b2e1be7ba1cd425
Milan Komůrka <118112129+666komo@users.noreply.github.com>
2023-11-08 18:37:28 +0100

    negriprdel

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
     const loginForm = document.getElementById("login-form");
     const errorMessage = document.getElementById("error-message");
+    const passwordField = document.getElementById("password");
+    const showPasswordCheckbox = document.getElementById("show-password");
+
+    // Function to toggle password visibility
+    showPasswordCheckbox.addEventListener("change", function () {
+        passwordField.type = showPasswordCheckbox.checked ? "text" : "password";
+    });
 
     loginForm.addEventListener("submit", function (e) {
         e.preventDefault();
         const username = document.getElementById("username").value;
-        const password = document.getElementById("password").value;
+        const password = passwordField.value;
 
         // Perform simple username and password validation
         if (username === "pwalker" && password === "skyline") {
