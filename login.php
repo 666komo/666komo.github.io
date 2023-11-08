6320862f7a8a58909d9dfa6c791230db22c8a94f
Milan Komůrka <118112129+666komo@users.noreply.github.com>
2023-11-08 18:20:11 +0100

    negr

---
+++ login.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+// Define the correct username and password
+$correct_username = "pwalker";
+$correct_password = "skyline";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    if ($username === $correct_username && $password === $correct_password) {
+        // Successful login
+        $_SESSION["username"] = $username;
+        header("Location: https://www.seznam.cz"); // Redirect to seznam.cz
+        exit;
+    } else {
+        // Failed login
+        header("Location: login.html"); // Redirect back to the login screen
+        exit;
+    }
+}
+?>
